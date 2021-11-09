070108cd719b2441884ab4a907a017608732fea4
Dennis Nguyen <dennisnguyen40@gmail.com>
2021-11-08 23:51:56 -0800

    integrated selection sort

--- tables.js
+++ tables.js
@@ -1,21 +1,46 @@
-const chosenAlgo = localStorage.getItem("chosenAlgo");
-var resetValue = null;
-localStorage.setItem("chosenAlgo", resetValue);
-
+// const chosenAlgo = localStorage.getItem("chosenAlgo");
+const chosenAlgo = localStorage.chosenAlgo
+console.log(chosenAlgo)
+let queryParam = null
 
 var container = document.getElementById("array");
+
+
+generatearray();
+
+
+
+// set query Param to fetch scrollText from Dipan's microservice
+if (chosenAlgo == 'bubble_sort') {
+    queryParam = 'bubble-sort';
+    BubbleSort();
+} else if (chosenAlgo == 'selection_sort') {
+    queryParam = 'selection-sort';
+    SelectionSort();
+} else if (chosenAlgo == 'insertion_sort') {
+    queryParam = 'insertion-sort'
+} else if (chosenAlgo == 'quick_sort') {
+    queryParam = 'quick-sort'
+} else if (chosenAlgo == 'merge_sort') {
+    queryparam = 'merge-sort'
+}
+
+
+
+
+
   
 // Function to generate the array of blocks
 function generatearray() {
     for (var i = 0; i < 20; i++) {
   
-        // Return a value from 1 to 100 (both inclusive)
+        // return random value between 1 and 100
         var value = Math.ceil(Math.random() * 100);
   
-        // Creating element div
+        // create div element
         var array_ele = document.createElement("div");
   
-        // Adding class 'block' to div
+
         array_ele.classList.add("block");
   
         // Adding style to div
@@ -96,16 +121,80 @@ async function BubbleSort(delay = 100) {
     }
 }
   
-// Calling generatearray function
-generatearray();
-  
-// Calling BubbleSort function
-BubbleSort();
+// asynchronous function to perform "Selection Sort"
+async function SelectionSort(delay = 300) {
+    let blocks = document.querySelectorAll(".block");
+    // Assign 0 to min_idx
+     var min_idx = 0;
+     for (var i = 0; i < blocks.length; i += 1) {
+    
+      // Assign i to min_idx
+      min_idx = i;
+    
+      // Provide darkblue color to the ith bar
+      blocks[i].style.backgroundColor = "darkblue";
+      for (var j = i + 1; j < blocks.length; j += 1) {
+    
+        // Provide red color to the jth bar
+        blocks[j].style.backgroundColor = "red";
+          
+        // To pause the execution of code for 300 milliseconds
+        await new Promise((resolve) =>
+          setTimeout(() => {
+            resolve();
+          }, 300)
+        );
+    
+        // To store the integer value of jth bar to var1 
+        var val1 = parseInt(blocks[j].childNodes[0].innerHTML);
+    
+        // To store the integer value of (min_idx)th bar to var2 
+        var val2 = parseInt(blocks[min_idx].childNodes[0].innerHTML);
+          
+        // Compare val1 & val2
+        if (val1 < val2) {
+          if (min_idx !== i) {
+    
+            // Provide skyblue color to the (min-idx)th bar
+            blocks[min_idx].style.backgroundColor = "blue";
+          }
+          min_idx = j;
+        } else {
+    
+          // Provide skyblue color to the jth bar
+          blocks[j].style.backgroundColor = "blue";
+        }
+      }
+    
+      // To swap ith and (min_idx)th bar
+      var temp1 = blocks[min_idx].style.height;
+      var temp2 = blocks[min_idx].childNodes[0].innerText;
+      blocks[min_idx].style.height = blocks[i].style.height;
+      blocks[i].style.height = temp1;
+      blocks[min_idx].childNodes[0].innerText = blocks[i].childNodes[0].innerText;
+      blocks[i].childNodes[0].innerText = temp2;
+        
+      // To pause the execution of code for 300 milliseconds
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          resolve();
+        }, 300)
+      );
+    
+      // Provide skyblue color to the (min-idx)th bar
+      blocks[min_idx].style.backgroundColor = "blue";
+    
+      // Provide lightgreen color to the ith bar
+      blocks[i].style.backgroundColor = "green";
+    }
+    
+  }
 
 
 const fetchScrollText = async() => {
     try {
-        const res = await axios.get('https://microservice-dipan.herokuapp.com/scrape?algorithm=bubble-sort')
+        const res = await axios.get(`https://microservice-dipan.herokuapp.com/scrape?algorithm=` + queryParam)
+        console.log(res)
         const text = res.data.description;
         return text;
     } catch(e) {
@@ -128,3 +217,10 @@ const addScrollText = async() => {
 }
 
 addScrollText();
+
+
+
+
+// var resetValue = null;
+// // reset chosenAlgo variable to null
+// localStorage.setItem("chosenAlgo", resetValue);
