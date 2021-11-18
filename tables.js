52dbc3e731b628e4836d069e3983426abec86cf9
Dennis Nguyen <dennisnguyen40@gmail.com>
2021-11-18 14:15:13 -0800

    cleaned up comments and added insertion sort

--- tables.js
+++ tables.js
@@ -18,7 +18,8 @@ if (chosenAlgo == 'bubble_sort') {
     queryParam = 'selection-sort';
     SelectionSort();
 } else if (chosenAlgo == 'insertion_sort') {
-    queryParam = 'insertion-sort'
+    queryParam = 'insertion-sort';
+    InsertionSort();
 } else if (chosenAlgo == 'quick_sort') {
     queryParam = 'quick-sort'
 } else if (chosenAlgo == 'merge_sort') {
@@ -79,20 +80,18 @@ function swap(el1, el2) {
     });
 }
   
-// Asynchronous BubbleSort function
+// async bubblesort
 async function BubbleSort(delay = 100) {
     var blocks = document.querySelectorAll(".block");
-  
-    // BubbleSort Algorithm
+
     for (var i = 0; i < blocks.length; i += 1) {
         for (var j = 0; j < blocks.length - i - 1; j += 1) {
   
-            // To change background-color of the
-            // blocks to be compared
+            // change color of blocks currently being prepared
             blocks[j].style.backgroundColor = "blue";
             blocks[j + 1].style.backgroundColor = "blue";
   
-            // To wait for .1 sec
+            // set 0.1 sec delay
             await new Promise((resolve) =>
                 setTimeout(() => {
                     resolve();
@@ -103,70 +102,66 @@ async function BubbleSort(delay = 100) {
             var value1 = Number(blocks[j].childNodes[0].innerHTML);
             var value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
   
-            // To compare value of two blocks
+            // compare values
             if (value1 > value2) {
                 await swap(blocks[j], blocks[j + 1]);
                 blocks = document.querySelectorAll(".block");
             }
   
-            // Changing the color to the previous one
+            // change color back
             blocks[j].style.backgroundColor = "gray";
             blocks[j + 1].style.backgroundColor = "gray";
         }
   
-        //changing the color of greatest element 
-        //found in the above traversal
+        // color change to identify max found in current pass
         blocks[blocks.length - i - 1]
                 .style.backgroundColor = "green";
     }
 }
   
-// asynchronous function to perform "Selection Sort"
+// async selection sort 
 async function SelectionSort(delay = 300) {
     let blocks = document.querySelectorAll(".block");
-    // Assign 0 to min_idx
+
      var min_idx = 0;
      for (var i = 0; i < blocks.length; i += 1) {
     
-      // Assign i to min_idx
       min_idx = i;
     
-      // Provide darkblue color to the ith bar
+      // ith bar is dark blue
       blocks[i].style.backgroundColor = "darkblue";
-      for (var j = i + 1; j < blocks.length; j += 1) {
+      for (let j = i + 1; j < blocks.length; j += 1) {
     
-        // Provide red color to the jth bar
+        // jth bar is red
         blocks[j].style.backgroundColor = "red";
           
-        // To pause the execution of code for 300 milliseconds
+        // set 300 ms delay
         await new Promise((resolve) =>
           setTimeout(() => {
             resolve();
           }, 300)
         );
-    
-        // To store the integer value of jth bar to var1 
-        var val1 = parseInt(blocks[j].childNodes[0].innerHTML);
-    
-        // To store the integer value of (min_idx)th bar to var2 
-        var val2 = parseInt(blocks[min_idx].childNodes[0].innerHTML);
+     
+        let val1 = parseInt(blocks[j].childNodes[0].innerHTML);
+     
+        let val2 = parseInt(blocks[min_idx].childNodes[0].innerHTML);
           
-        // Compare val1 & val2
+
         if (val1 < val2) {
           if (min_idx !== i) {
     
-            // Provide skyblue color to the (min-idx)th bar
+            // min indexth bar is blue
             blocks[min_idx].style.backgroundColor = "blue";
           }
           min_idx = j;
         } else {
     
-          // Provide skyblue color to the jth bar
+          // jth bar is blue
           blocks[j].style.backgroundColor = "blue";
         }
       }
     
-      // To swap ith and (min_idx)th bar
+      // swap ith and (min_idx)th block
       var temp1 = blocks[min_idx].style.height;
       var temp2 = blocks[min_idx].childNodes[0].innerText;
       blocks[min_idx].style.height = blocks[i].style.height;
@@ -174,27 +169,95 @@ async function SelectionSort(delay = 300) {
       blocks[min_idx].childNodes[0].innerText = blocks[i].childNodes[0].innerText;
       blocks[i].childNodes[0].innerText = temp2;
         
-      // To pause the execution of code for 300 milliseconds
       await new Promise((resolve) =>
         setTimeout(() => {
           resolve();
         }, 300)
       );
     
-      // Provide skyblue color to the (min-idx)th bar
+      // min-indexth bar turns blue
       blocks[min_idx].style.backgroundColor = "blue";
     
-      // Provide lightgreen color to the ith bar
+      // ith bar turns green
       blocks[i].style.backgroundColor = "green";
     }
     
+  }
+
+  async function InsertionSort(delay = 600) {
+    let blocks = document.querySelectorAll(".block");
+    
+    // 0th bar is lightgreen
+    blocks[0].style.backgroundColor = " rgb(49, 226, 13)";
+    for (var i = 1; i < blocks.length; i += 1) {
+
+      let j = i - 1;
+    
+      let key = 
+      parseInt(blocks[i].childNodes[0].innerHTML);
+    
+      let height = blocks[i].style.height;
+       
+    //   // For selecting section having id "ele"
+    //   var barval=document.getElementById("ele")
+    
+    //   // For dynamically Updating the selected element
+    //     barval.innerHTML=
+    //     `<h3>Element Selected is :${key}</h3>`;
+     
+      blocks[i].style.backgroundColor = "darkblue";
+        
+      await new Promise((resolve) =>
+      setTimeout(() => {
+        resolve();
+      }, 600)
+    );
+    
+      // For placing selected element at its correct position 
+      while (j >= 0 && parseInt(blocks[j].childNodes[0].innerHTML) > key) {
+          
+        // jth bar is darkblue
+        blocks[j].style.backgroundColor = "darkblue";
+          
+        // swap jth and (j+1)th element
+        blocks[j + 1].style.height = blocks[j].style.height;
+        blocks[j + 1].childNodes[0].innerText = 
+        blocks[j].childNodes[0].innerText;
+    
+        j = j - 1;
+    
+        await new Promise((resolve) =>
+          setTimeout(() => {
+            resolve();
+          }, 300)
+        );
+          
+        // turn sorted portion lightgreen
+        for(let k=i;k>=0;k--){
+          blocks[k].style.backgroundColor = " rgb(49, 226, 13)";
+        }
+      }
+    
+      // move selected element to correct position
+      blocks[j + 1].style.height = height;
+      blocks[j + 1].childNodes[0].innerHTML = key;
+         
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          resolve();
+        }, 300)
+      );
+        
+      // Provide light green color to the ith bar
+      blocks[i].style.backgroundColor = " rgb(49, 226, 13)";
+    }
   }
 
 
 const fetchScrollText = async() => {
     try {
         const res = await axios.get(`https://microservice-dipan.herokuapp.com/scrape?algorithm=` + queryParam)
-        console.log(res)
+        console.log(res.data)
         const text = res.data.description;
         return text;
     } catch(e) {
